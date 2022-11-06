7f9f5a5fbf0f75a8647286e5b04653faf43d29e8
kyoto08 <dinudarii8@gmail.com>
2022-11-06 23:56:36 +0200

    now the app is working

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, {useState} from "react";
-import Header from "./components/Header";
-import Form from "./components/Form";
-import TodoList from "./components/TodoList";
-import "./App.css";
+import "./App.css"
 
-const App = () => {
+function App() {
+    const[list, setList] = useState([]);
+    const[input, setInput] = useState("");
 
-    const [input, setInput] = useState("");
-    const [todos, setTodos] = useState([]);
-    return (
-    <div className="container">
-        <div className="app-wrapper">    
-            <div>
-                <Header />
-            </div>
-            <div>
-                <Form
-                input = {input}
-                setInput = {setInput}
-                todos = {todos}
-                setTodos = {setTodos}
+    const addTodo = (todo) => {
+        const newTodo = {
+            id: Math.random(),
+            todo: todo
+        }
+        setList([...list, newTodo])
+        setInput("")
+    }
+
+    const deleteTodo = (id) => {
+        const newList = list.filter((todo)=>todo.id !== id);
+        setList(newList);
+    };
+
+    return (<div>
+                <h1>To Do List</h1>
+                <input
+                type="text"
+                value={input}
+                onChange = {(e) => setInput(e.target.value)}
                 />
-            </div>
-            <div>
-                <TodoList todos = {todos}
-                setTodos = {setTodos}/>
-            </div>
-        </div>
-    </div>);
+                <button onClick={() => addTodo(input)}>Add Task</button>
+                <ul>
+                    {list.map((todo) => (
+                        <li key={todo.id}>
+                            {todo.todo}
+                            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>);
 }
 
 export default App;
